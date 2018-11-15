c2b39f2089d46b844b43582c243a58b636a633ab
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-15 10:46:56 +0100

    Committing changes

--- sdm.assignment9.Core.Entity/Class1.cs
+++ sdm.assignment9.Core.Entity/Class1.cs
@@ -4,5 +4,11 @@ namespace sdm.assignment9.Core.Entity
 {
     public class Class1
     {
+
+        public void DoTheThing()
+        {
+            int id;
+            id = 1;
+        }
     }
 }
